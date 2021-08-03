45cd47e821190cf3a4e65129980885cfcab1094f
叶锦湛 <jinzhan.ye@foxmail.com>
2021-08-03 13:45:47 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <div>
       <p>
         If Element is successfully added to this project, you'll see an
@@ -10,17 +9,116 @@
       <el-button>el-button</el-button>
       <a-button type="primary">Ant Primary</a-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div style="margin-bottom: 30px">
+      <h2>a form</h2>
+      <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" @submit="handleSubmit">
+        <a-form-item label="Note">
+          <a-input
+              v-decorator="[
+              'note',
+              {
+                rules: [{ required: true, message: 'Please input your note!' }],
+                initialValue: 'I am initialValue'
+              }
+              ]"/>
+        </a-form-item>
+
+        <a-form-item label="Gender">
+          <a-select
+              v-decorator="[
+                  'gender',
+                  { rules: [{ required: true, message: 'Please select your gender!' }] },
+                  ]"
+              placeholder="Select a option and change input text above"
+              @change="handleSelectChange">
+            <a-select-option value="male">
+              male
+            </a-select-option>
+            <a-select-option value="female">
+              female
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+
+        <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+          <a-button type="primary" html-type="submit">
+            Submit
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+
+    <div>
+      <h2>el form</h2>
+      <el-form :model="numberValidateForm" ref="numberValidateForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item
+            label="姓名"
+            prop="name"
+            :rules="[{ required: true, message: '姓名不能为空'},]">
+          <el-input v-model="numberValidateForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item
+            label="年龄"
+            prop="age"
+            :rules="[
+                { required: true, message: '年龄不能为空'},
+                { type: 'number', message: '年龄必须为数字值'}]">
+          <el-input type="age" v-model.number="numberValidateForm.age" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('numberValidateForm')">提交</el-button>
+          <el-button @click="resetForm('numberValidateForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      formLayout: 'horizontal',
+      form: this.$form.createForm(this, { name: 'coordinated' }),
+      numberValidateForm: {
+        name: '',
+        age: '',
+      }
+    }
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault()
+
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values)
+        }
+      })
+    },
+    handleSelectChange(value) {
+      console.log(value)
+
+      this.form.setFieldsValue({
+        note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+      })
+    },
+
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log(this.numberValidateForm)
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   }
 }
 </script>
